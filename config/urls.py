45e37b1aad844da7b4bb2038fca96622439d6a13
sunkyu_park <noncefaln@gmail.com>
2022-07-19 23:16:43 +0900

    7th commit

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
-    # path('content/upload', UploadFeed.as_view())
-    path('', include('content.urls'))
+    path('content/', include('content.urls')),
+    path('user/', include('user.urls'))
 ]
 
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -4,6 +4,19 @@ from rest_framework.views import APIView
 # Create your views here.
 
 
-class join(APIView):
+class Join(APIView):
     def get(self, request):
         return render(request, "user/join.html")
+
+    def post(self, request):
+        # TODO 회원가입
+        pass
+
+
+class Login(APIView):
+    def get(self, request):
+        return render(request, "user/login.html")
+
+    def post(self, request):
+        # TODO 로그인
+        pass
