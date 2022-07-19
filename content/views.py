ba0f4a8cb48f6b921fb9b476be31c29fed5aaa5c
sunkyu_park <noncefaln@gmail.com>
2022-07-19 11:14:43 +0900

    5th commit

--- content/views.py
+++ content/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import Feed
+from content.models import Feed
+from rest_framework.response import Response
+import os
+# from .settings import MEDIA_ROOT
+from uuid import uuid4
 
 
 class Main(APIView):
     def get(self, request):
         feed_list = Feed.objects.all().order_by('-id')  # select * from content_feed;
-
-        for feed in feed_list:
-            print(feed.content)
-
         return render(request, "jinstagram/main.html", context=dict(feeds=feed_list))
+
+
+class UploadFeed(APIView):
+    def post(self, request):
+
+        file = request.data.get('file')
+        image = request.data.get('image')
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image = request.data.get('profile_image')
