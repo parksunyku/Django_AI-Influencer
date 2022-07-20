45829184ba505d4fc60a7edcdb6e2209e94a00bb
sunkyu_park <noncefaln@gmail.com>
2022-07-20 16:08:29 +0900

    8th commit

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from requests import Response
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import User
 from django.contrib.auth.hashers import make_password
@@ -17,8 +17,9 @@ class Join(APIView):
         name = request.data.get('name', None)
         password = request.data.get('password', None)
 
-        User.objects.create(email=email, nickname=nickname,
-                            name=name, password=make_password(password), profile_image="default_profile.jpg")
+        User.objects.create(password=make_password(
+            password), name=name, email=email, nickname=nickname, profile_image="dddd.jpg")
+
         return Response(status=200)
 
 
@@ -28,4 +29,16 @@ class Login(APIView):
 
     def post(self, request):
         # TODO 로그인
-        pass
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return Response(status=400, data=dict(message="회원정보가 잘못되었습니다"))
+
+        if user.check_password(password):
+            # TODO 로그인을 했다. 세션 or 쿠키에 넣는다.
+            return Response(status=200)
+        else:
+            return Response(status=400, data=dict(message="회원정보가 잘못되었습니다"))
