8687e1638cc56e2ae29af6354116eb0d2d43b49a
sunkyu_park <noncefaln@gmail.com>
2022-07-22 17:48:22 +0900

    11th commit

--- content/models.py
+++ content/models.py
@@ -6,6 +6,5 @@ from django.db import models
 class Feed(models.Model):
     content = models.TextField()         # 글 내용
     image = models.TextField()           # 피드 이미지
-    profile_image = models.TextField()   # 프로필 이미지
-    user_id = models.TextField()         # 글쓴이
+    email = models.TextField(default='')        # 글쓴이
     like_count = models.IntegerField()  # 좋아요 수
