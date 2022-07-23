9a12369215475253661d9aeaede8683bbcec5eb2
sunkyu_park <noncefaln@gmail.com>
2022-07-23 09:56:23 +0900

    10th commit

--- content/models.py
+++ content/models.py
@@ -8,3 +8,21 @@ class Feed(models.Model):
     image = models.TextField()           # 피드 이미지
     email = models.TextField(default='')        # 글쓴이
     like_count = models.IntegerField()  # 좋아요 수
+
+
+class Like(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.TextField(default='')
+    is_like = models.BooleanField(default=True)
+
+
+class Reply(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_content = models.TextField()
+
+
+class Bookmark(models.Model):
+    content_id = models.IntegerField(default=0)
+    email = models.TextField(default='')
+    is_marked = models.BooleanField(default=True)
--- content/views.py
+++ content/views.py
@@ -1,12 +1,10 @@
-import email
-from django.shortcuts import render
-from rest_framework.views import APIView
-from content.models import Feed
-from rest_framework.response import Response
-import os
-from user.models import User
-from config. settings import MEDIA_ROOT
 from uuid import uuid4
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from content.models import Feed, Reply, Like, Bookmark
+from user.models import User
+import os
 from config.settings import MEDIA_ROOT
 
 
@@ -18,12 +16,20 @@ class Main(APIView):
 
         for feed in feed_object_list:
             user = User.objects.filter(email=feed.email).first()
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
+            reply_list = []
+            for reply in reply_object_list:
+                user = User.objects.filter(email=reply.email).first()
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       nickname=user.nickname))
 
-            feed_list.append(dict(image=feed.image,
+            feed_list.append(dict(id=feed.id,
+                                  image=feed.image,
                                   content=feed.content,
                                   like_count=feed.like_count,
                                   profile_image=user.profile_image,
-                                  nickname=user.nickname
+                                  nickname=user.nickname,
+                                  reply_list=reply_list
                                   ))
 
         email = request.session.get('email', None)
@@ -71,3 +77,15 @@ class Profile(APIView):
             return render(request, "user/login.html")
 
         return render(request, 'content/profile.html', context=dict(user=user))
+
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(
+            feed_id=feed_id, reply_content=reply_content, email=email)
+
+        return Response(status=200)
