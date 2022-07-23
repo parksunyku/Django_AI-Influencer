9995e1e60d71d1ee83458190f060d6bfc91b6560
sunkyu_park <noncefaln@gmail.com>
2022-07-23 12:11:51 +0900

    11th commit

--- content/models.py
+++ content/models.py
@@ -6,13 +6,13 @@ from django.db import models
 class Feed(models.Model):
     content = models.TextField()         # 글 내용
     image = models.TextField()           # 피드 이미지
-    email = models.TextField(default='')        # 글쓴이
+    email = models.EmailField(default='')        # 글쓴이
     like_count = models.IntegerField()  # 좋아요 수
 
 
 class Like(models.Model):
     feed_id = models.IntegerField(default=0)
-    email = models.TextField(default='')
+    email = models.EmailField(default='')
     is_like = models.BooleanField(default=True)
 
 
@@ -23,6 +23,6 @@ class Reply(models.Model):
 
 
 class Bookmark(models.Model):
-    content_id = models.IntegerField(default=0)
-    email = models.TextField(default='')
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
     is_marked = models.BooleanField(default=True)
