aa5a1fd8716c78fed7194ce11ce7acbd9f9a7693
sunkyu_park <noncefaln@gmail.com>
2022-07-31 13:11:13 +0900

    18th commit

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
 ALLOWED_HOSTS = ['52.78.122.148', 'ainf.shop']
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'config/static/'
 STATICFILES_DIRS = []
 DEBUG = False
