b8e4abffc3ffb2cfdecaa7ef9da409d3c8e44ead
sunkyu_park <noncefaln@gmail.com>
2022-07-31 22:29:32 +0900

    18th commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -185,8 +185,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'config/static', ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,8 @@
 from .base import *
+import os
 
 ALLOWED_HOSTS = ['52.78.122.148', 'ainf.shop']
-STATIC_ROOT = BASE_DIR / 'config/static/'
-STATICFILES_DIRS = ['*']
-DEBUG = True
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+DEBUG = False
