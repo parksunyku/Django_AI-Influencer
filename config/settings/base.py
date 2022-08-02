d9423cded061b290bb8e118fbbb7e435801b617e
sunkyu_park <noncefaln@gmail.com>
2022-08-02 16:49:55 +0900

    static 2차 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -185,10 +185,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+print("test=== ", BASE_DIR)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR + '/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
